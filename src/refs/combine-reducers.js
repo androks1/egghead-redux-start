835e62d78b8e2eced561e0129cb06624b4d41a5a
Andrzej Zaleski <azaleski23@gmail.com>
2017-01-16 00:24:18 +0100

    v16: Implementing combineReducers() from Scratch

---
+++ src/refs/combine-reducers.js
@@ -0,0 +1,38 @@
+// const todoApp = (state = {}, action) => {
+//   return {
+//      // Call the `todos()` reducer from last section
+//      todos: todos( 
+//       state.todos,
+//       action
+//     ),
+//     visibilityFilter: visibilityFilter(
+//       state.visibilityFilter,
+//       action
+//     )
+//   };
+// };
+
+// const { combineReducers } = Redux; // CDN Redux import
+
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+
+    // Reduce all the keys for reducers from `todos` and `visibilityFilter`
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        // Call the corresponding reducer function for a given key
+        nextState[key] = reducers[key] (
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {} // The `reduce` on our keys gradually fills this empty object until it is returned.
+    );
+  };
+};
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
