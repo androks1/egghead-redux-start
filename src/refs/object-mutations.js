b2cf9f69ad7710f583571c8fed45b60c78188497
Andrzej Zaleski <azaleski23@gmail.com>
2017-01-15 23:46:22 +0100

    v10: Avoiding Object Mutations with Object.assign() and ...spread

---
+++ src/refs/object-mutations.js
@@ -0,0 +1,38 @@
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const toggleTodo = (todo) => {
+  // Mutated version:
+  // todo.completed = !todo.completed
+  // Impmutable version:
+  // return Object.assign({}, toado, {
+  //   completed: !todo.completed
+  // });
+  // Object spread operator version:
+  return {
+    ...todo,
+    completed: !todo.completed
+  }
+}
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
+console.log('All tests passed.');
